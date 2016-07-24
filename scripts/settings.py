6e75dc4b33fe35d0c1dd9d829ad3f37e91636ba4
Martin <m.kersner@gmail.com>
2016-07-24 21:28:50 +0900

    import different classifiers and preprocessing methods

--- scripts/settings.py
+++ scripts/settings.py
@@ -5,6 +5,21 @@ Martin Kersner, m.kersner@gmail.com
 2016/07/24
 '''
 
+# Preprocessing
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import MaxAbsScaler, RobustScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+
+# Classifiers
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.ensemble import ExtraTreesClassifier as ET
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.ensemble import AdaBoostClassifier as AB
+from sklearn.ensemble import GradientBoostingClassifier as GB
+
 settings = {}
 settings["data_path_orig"] = "../data/{}/orig/{}"
 settings["train_csv_orig"] = "numerai_training_data.csv"
@@ -18,3 +33,7 @@ settings["to_save"] = True
 
 settings["test_size"] = 0.1
 settings["validation_size"] = 0.1
+
+# Training settings
+settings['model'] = RF()
+settings['transformers']  = [Pipeline([ ('poly', PolynomialFeatures()), ('scaler', MinMaxScaler()) ])]
