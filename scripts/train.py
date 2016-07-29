314bcb321ba008b67475f250f4f445b26297429a
Martin Keršner <martin@company100.com>
2016-07-29 17:25:21 +0900

    simple cross validation and prediction of tournament data

--- scripts/train.py
+++ scripts/train.py
@@ -7,7 +7,7 @@ Martin Kersner, m.kersner@gmail.com
 
 import sys
 import pandas as pd
-from sklearn import cross_validation as CV
+from sklearn import cross_validation
 import numpy as np
 
 from tools import *
@@ -22,28 +22,36 @@ def main():
     train_dataset = load_csv(train_path)
     model = train(train_dataset)
 
-    test_path = validate_test_dataset(sys.argv[1], orig=False)
-    test_dataset = load_csv(test_path)
+    #test_path = validate_test_dataset(sys.argv[1], orig=False)
+    #test_dataset = load_csv(test_path)
+    #test(model, test_dataset)
 
-    test(model, test_dataset)
+    tournament_path = validate_test_dataset(sys.argv[1], orig=True)
+    tournament_dataset = load_csv(tournament_path)
+    predict_tournament(model, tournament_dataset)
 
 def train(train_data):
   #train, test = load_data(train_path, test_path)
   #transformers = settings['transformers']
 
   model = settings['model']
-  model.set_params(n_jobs=2)
-  model.set_params(n_estimators=1000)
+  model.set_params(n_jobs=8)
+  model.set_params(n_estimators=100)
 
-  X, y_true = split2Xy(train_data)
-  clf = model.fit(X, y_true)
-  print clf.get_params
+  X, y = split2Xy(train_data)
+  scores = cross_validation.cross_val_score(model, X, y, cv=5, scoring='log_loss', verbose=1)
+  print("Cross-validation accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+  model.fit(X, y)
+
+  #clf = model.fit(X, y_true)
+  #print clf.get_params
   #print clf.feature_importances_
 
   #y_pred = clf.predict_proba(X)
   #print log_loss(y_true, y_pred)
 
-  return clf
+  return model
 
   #for transformer in transformers:
   #  print transformer
@@ -67,7 +75,21 @@ def test(model, test_data):
   X, y_true = split2Xy(test_data)
   y_pred = model.predict_proba(X)
 
-  print log_loss(y_true, y_pred)
+  scores = log_loss(y_true, y_pred)
+  print("Test set accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+def predict_tournament(model, data):
+  X = data.drop('t_id', axis=1)
+  y_pred = model.predict_proba(X)
+  data['probability'] = y_pred[:, 1] # TODO confirm the second column is correct
+
+  # generate submission
+  uniq_id = get_timestamp_str()
+  submission_path = '../submissions/' + uniq_id + '.csv'
+  model_path      = '../models/'      + uniq_id + '.pkl'
+
+  data.to_csv(submission_path, columns=('t_id', 'probability'), index=False)
+  save_model(model, model_path)
 
 def experiment(dataset):
   model = settings['model']
