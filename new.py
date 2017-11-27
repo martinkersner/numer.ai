0f0583a3a6cb1bfe9188de27ce5ea73187a69eb0
Martin Kersner <m.kersner@gmail.com>
2017-11-28 00:12:27 +0900

    defining own tf estimator

--- new.py
+++ new.py
@@ -1,5 +1,7 @@
+import sys
 import collections
 import tensorflow as tf
+import argparse
 
 defaults = collections.OrderedDict([
     ("id", [""]),
@@ -62,7 +64,6 @@ defaults = collections.OrderedDict([
 tf.logging.set_verbosity(tf.logging.INFO)
 
 def my_input_fn(file_path, perform_shuffle=False, repeat_count=1):
-
     def _decode_line(line):
         items = tf.decode_csv(line, list(defaults.values()))
         pairs = zip(defaults.keys(), items)
@@ -84,29 +85,94 @@ def my_input_fn(file_path, perform_shuffle=False, repeat_count=1):
     return features, labels
 
 
-# FILE_TRAIN = "numerai_training_data.csv"
-FILE_TRAIN = "numerai_training_data_head.csv"
-FILE_TEST = "numerai_training_data_head.csv"
+def process_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--training_data", type=str, default="numerai_training_data.csv", help="Path to the training data.")
+    parser.add_argument(
+        "--tournament_data", type=str, default="numerai_tournament_data.csv", help="Path to the test data.")
+    return parser.parse_args()
 
-feature_columns = [tf.feature_column.numeric_column(k) for k in defaults.keys() if "feature" in k]
 
-estimator = tf.estimator.DNNRegressor(
-    feature_columns=feature_columns,
-    hidden_units=[10, 10],
-    optimizer=tf.train.AdamOptimizer(),
-    model_dir="logdir")
+def model_fn(features, labels, mode, params):
+    feature_columns = [tf.feature_column.numeric_column(k) for k in defaults.keys() if "feature" in k]
+    input_layer = tf.feature_column.input_layer(
+        features=features, feature_columns=feature_columns)
 
-estimator.train(
-    input_fn=lambda: my_input_fn(FILE_TRAIN, True, 1))
+    second_hidden_layer = tf.layers.dense(input_layer, 10, activation=tf.nn.relu)
 
-evaluate_result = estimator.evaluate(
-    input_fn=lambda: my_input_fn(FILE_TEST, False, 1))
+    output_layer = tf.layers.dense(second_hidden_layer, 1)
 
-for key in evaluate_result:
-    print("   {}: {}".format(key, evaluate_result[key]))
+    predictions = tf.reshape(output_layer, [-1])
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions={"target": predictions})
 
-predict_results = estimator.predict(
-    input_fn=lambda: my_input_fn(FILE_TEST, False, 1))
+    # loss = tf.losses.log_loss(labels, predictions)
+    loss = tf.losses.mean_squared_error(labels, predictions)
 
-for prediction in predict_results:
-    print(prediction["predictions"][0])
+    eval_metric_ops = {
+    "rmse": tf.metrics.root_mean_squared_error(
+        tf.cast(labels, tf.float32), predictions)
+    }
+
+    optimizer = tf.train.GradientDescentOptimizer(
+        learning_rate=params["learning_rate"])
+    train_op = optimizer.minimize(
+        loss=loss, global_step=tf.train.get_global_step())
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops=eval_metric_ops)
+
+
+
+def main():
+    args = process_arguments()
+    FILE_TRAIN = args.training_data
+    FILE_TEST = args.tournament_data
+
+    FILE_TRAIN = "numerai_training_data_head.csv"
+    FILE_TEST = "numerai_training_data_head.csv"
+
+    model_params = {"learning_rate": 0.001}
+    nn = tf.estimator.Estimator(model_fn=model_fn, params=model_params)
+
+    nn.train(
+        input_fn=lambda: my_input_fn(FILE_TRAIN, True, 1), steps=2000)
+
+    nn.evaluate(
+        input_fn=lambda: my_input_fn(FILE_TRAIN, False, 1))
+
+    predictions = nn.predict(
+        input_fn=lambda: my_input_fn(FILE_TRAIN, False, 1))
+
+    for prediction in predictions:
+        print(prediction["target"])
+
+if __name__ == "__main__":
+    main()
+
+# def tmp(fFILE_TRAIN, FILE_TEST):
+    # feature_columns = [tf.feature_column.numeric_column(k) for k in defaults.keys() if "feature" in k]
+    # estimator = tf.estimator.DNNRegressor(
+        # feature_columns=feature_columns,
+        # hidden_units=[10, 10],
+        # optimizer=tf.train.AdamOptimizer(),
+        # model_dir="logdir")
+
+    # estimator.train(
+        # input_fn=lambda: my_input_fn(FILE_TRAIN, True, 1))
+
+    # evaluate_result = estimator.evaluate(
+        # input_fn=lambda: my_input_fn(FILE_TEST, False, 1))
+
+    # for key in evaluate_result:
+        # print("   {}: {}".format(key, evaluate_result[key]))
+
+    # predict_results = estimator.predict(
+        # input_fn=lambda: my_input_fn(FILE_TEST, False, 1))
+
+    # for prediction in predict_results:
+        # print(prediction["predictions"][0])
